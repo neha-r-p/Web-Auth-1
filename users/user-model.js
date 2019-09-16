c7a160ab1c128abc01c93c364403f1ff1d7cc4b1
Neha Patel <neha.r.p@gmail.com>
2019-09-16 14:04:00 -0700

    writing out user-model.js

---
+++ users/user-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById
+}
+
+function add () {
+
+}
+
+function find() {
+
+}
+
+function findBy() {
+
+}
+
+function findById () {
+    
+}

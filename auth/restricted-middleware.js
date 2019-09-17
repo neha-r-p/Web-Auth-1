a4aeda32c7a2eace00d98f0310daba951877fdfd
Neha Patel <neha.r.p@gmail.com>
2019-09-17 14:22:46 -0700

    modified middleware for session

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,30 +1,7 @@
-const bcrypt = require('bcryptjs')
-
-const Users = require('../users/user-model')
-
-module.exports = (req, res, next) => {
-  let {username, password} = req.headers;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if(user && bcrypt.compareSync(password, user.password)){
-        next();
-      } else {
-        res.status(401).json({ error: "You shall not pass!" })
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: "Could not find account" })
-    })
-}
-
-function fetch() {
-  const reqOptions = {
-    headers: {
-      username: "",
-      password: ""
-    }
+module.exports = function(req, res, next) {
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ error: "You shall not pass!" });
   }
-}
+};

c3ed954f415e0ddda27163b8c450e96969faf0f7
Neha Patel <neha.r.p@gmail.com>
2019-09-17 13:41:23 -0700

    day 2 cookies

---
+++ client/src/components/Register.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <form>
+            <h2>Register:</h2>
+            <label> Username:
+             <input type="text" placeholder="username" />   
+            </label>
+            <label>Password:<input type="password" placeholder="password" /></label>
+            
+        </form>
+    )
+}
+
+export default Register;

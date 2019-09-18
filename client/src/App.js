b0ba5c9a9f32665fea6e8b4439f53fe75bdcd683
Neha Patel <neha.r.p@gmail.com>
2019-09-17 21:53:38 -0700

    simple header and nav bar set up. need router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.scss';
-import Register from './components/Register'
+import React from "react";
+import "./App.scss";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
-     <h1>Testing!</h1>
-     <Register />
+      <div className="header">
+        <h1>Web Auth</h1>
+        <div className="navBar">
+          <a href="#">Register</a>
+          <a href="#">Users</a>
+          <a href="#">Login</a>
+          <a href="#">Logout</a>
+        </div>
+      </div>
+      <Register />
     </div>
   );
 }
